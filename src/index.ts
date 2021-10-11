330dfe9f958bf7030ff658f8361c0feb5debc6a9
aplave <aaron.plave@jpl.nasa.gov>
2021-10-11 11:20:59 -0700

    Export various components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import './styles/styles.scss'
 export * from "./components/Button";
+export * from "./components/Typography";
+export * from "./components/Icons";
+export * from "./components/Multiselect";
 export * from "./contexts/theme";
