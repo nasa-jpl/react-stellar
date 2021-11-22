7c171ba3f4edb7c730a1909b7cabab48b8074328
aplave <aaron.plave@jpl.nasa.gov>
2021-11-22 14:50:50 -0800

    Improve docs

--- src/stories/Popover.stories.tsx
+++ src/stories/Popover.stories.tsx
@@ -44,51 +44,47 @@ Interactive.args = {
   trigger: <Button>Open Popover</Button>,
 };
 
-export const OverridingRadixProps = Template.bind({});
-OverridingRadixProps.args = {
-  // All other props are spread to the Radix Popover Root.
-  // See https://www.radix-ui.com/docs/primitives/components/popover#root
-  // for a full list of Root props
-  modal: true, // Radix Popover Root prop example
-  onOpenChange: action("open changed"), // Radix Popover Root prop example
-
-  // Control the majority of the Popover behaviors by setting Radix Popover Content props.
-  // See https://www.radix-ui.com/docs/primitives/components/popover#content
-  // for a full list of Content props.
-  // Common props include "side", "sideOffset", "align", and "avoidCollisions".
-  contentProps: {
-    sideOffset: 18,
-    alignOffset: 32,
-    onInteractOutside: (evt) => evt.preventDefault(),
-    onPointerDownOutside: (evt) => evt.preventDefault(),
-    onEscapeKeyDown: (evt) => evt.preventDefault(),
-  },
-
-  children: (
-    <>
-      <div>
-        <div className="st-typography-small-caps">Option Group 1</div>
-        <div className="st-typography-medium">Checkbox 1 with label</div>
-        <div className="st-typography-medium">Checkbox 2 with label</div>
-        <div className="st-typography-medium">Checkbox 3 with label</div>
-      </div>
-      <br />
-      <div>
-        <div className="st-typography-small-caps">Option Group 2</div>
-        <div className="st-typography-medium">Checkbox 1 with label</div>
-        <div className="st-typography-medium">Checkbox 2 with label</div>
-        <div className="st-typography-medium">Checkbox 3 with label</div>
-      </div>
-      <br />
-      <PopoverClose asChild>
-        <Button variant="secondary" style={{ width: "100%" }}>
-          Close
-        </Button>
-      </PopoverClose>
-    </>
-  ),
-  trigger: <Button>Open Popover</Button>,
-};
+export const OverridingRadixProps = () => (
+  <Popover
+    // All other props are spread to the Radix Popover Root.
+    // See https://www.radix-ui.com/docs/primitives/components/popover#root
+    // for a full list of Root props
+    modal // Radix Popover Root prop example
+    onOpenChange={action("open changed")} // Radix Popover Root prop example
+    // Control the majority of the Popover behaviors by setting Radix Popover Content props.
+    // See https://www.radix-ui.com/docs/primitives/components/popover#content
+    // for a full list of Content props.
+    // Common props include "side", "sideOffset", "align", and "avoidCollisions".
+    contentProps={{
+      sideOffset: 18,
+      alignOffset: 32,
+      onInteractOutside: (evt) => evt.preventDefault(),
+      onPointerDownOutside: (evt) => evt.preventDefault(),
+      onEscapeKeyDown: (evt) => evt.preventDefault(),
+    }}
+    trigger={<Button>Open Popover</Button>}
+  >
+    <div>
+      <div className="st-typography-small-caps">Option Group 1</div>
+      <div className="st-typography-medium">Checkbox 1 with label</div>
+      <div className="st-typography-medium">Checkbox 2 with label</div>
+      <div className="st-typography-medium">Checkbox 3 with label</div>
+    </div>
+    <br />
+    <div>
+      <div className="st-typography-small-caps">Option Group 2</div>
+      <div className="st-typography-medium">Checkbox 1 with label</div>
+      <div className="st-typography-medium">Checkbox 2 with label</div>
+      <div className="st-typography-medium">Checkbox 3 with label</div>
+    </div>
+    <br />
+    <PopoverClose asChild>
+      <Button variant="secondary" style={{ width: "100%" }}>
+        Close
+      </Button>
+    </PopoverClose>
+  </Popover>
+);
 
 export const ReactJSXExample = () => (
   <Popover trigger={<Button>Open Popover</Button>}>
