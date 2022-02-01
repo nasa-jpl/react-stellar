750820f6b5a5564bc0db42cd58d7e4ce11a0665d
bduran <bryan.duran@jpl.nasa.gov>
2022-01-31 16:29:32 -0800

    added classname to formfield consistent formatting

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -3,13 +3,15 @@ import React from "react";
 
 export interface FormFieldProps {
   children: React.ReactNode;
+  className?: string;
   flow: "horizontal" | "vertical";
 }
 
-export const FormField = ({ flow, children }: FormFieldProps) => {
+export const FormField = ({ flow, className, children }: FormFieldProps) => {
   return (
     <div
       className={classNames("st-react-form-field", {
+        ...(className ? { [className]: true } : {}),
         ...(flow === "horizontal"
           ? { "st-react-form-field--horizontal": true }
           : { "st-react-form-field--vertical": true }),
