29acc6d18950e7290517542eceb8fec61bf07d57
AaronPlave <aaronplave@gmail.com>
2023-05-11 16:52:19 -0700

    Updates for Alert and Modal stories

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -1,19 +1,19 @@
 import { action } from "@storybook/addon-actions";
-import { StoryFn, Meta } from "@storybook/react";
-
+import { Meta, StoryObj } from "@storybook/react";
 import { Alert, AlertAction, AlertCancel } from "components/Alert";
 import { Button } from "index";
 
-export default {
-  component: Alert,
-} as Meta<typeof Alert>;
+export default { component: Alert } as Meta<typeof Alert>;
+type Story = StoryObj<typeof Alert>;
 
-export const Controlled = {
-  parameters: { docs: { disable: true } },
-
+export const Default: Story = {
   args: {
-    open: true,
     title: "Are you sure?",
+    trigger: (
+      <Button size="large" onClick={() => {}}>
+        Open Alert
+      </Button>
+    ),
     description:
       "This action cannot be undone, are you sure you want to delete this item?",
     onOpenChange: action("open changed"),
@@ -32,56 +32,26 @@ export const Controlled = {
   },
 };
 
-export const OverrideContentProps = {
+export const OverrideContentProps: Story = {
   parameters: { docs: { disable: true } },
-
   args: {
-    title: "Are you sure?",
-    trigger: (
-      <Button size="large" onClick={() => {}}>
-        Open Alert
-      </Button>
-    ),
-    description:
-      "Escape has been overridden to not automatically close the alert.",
-    onOpenChange: action("open changed"),
+    ...Default.args,
     alertContentProps: {
       onEscapeKeyDown: (evt) => {
         evt.preventDefault();
       },
     },
-    children: (
-      <>
-        <AlertCancel asChild>
-          <Button variant="secondary" onClick={action("action")}>
-            Cancel
-          </Button>
-        </AlertCancel>
-        <AlertAction asChild>
-          <Button onClick={action("action")}>Delete</Button>
-        </AlertAction>
-      </>
-    ),
+    description:
+      "Escape has been overridden to not automatically close the alert.",
+    title: "Are you sure?",
   },
 };
 
-export const ReactJSXExample = () => (
-  <Alert
-    title="Are you sure?"
-    description="This action cannot be undone, are you sure you want to delete this item?"
-    trigger={
-      <Button size="large" onClick={() => {}}>
-        Open Alert
-      </Button>
-    }
-  >
-    <AlertCancel asChild>
-      <Button variant="secondary" onClick={action("action")}>
-        Cancel
-      </Button>
-    </AlertCancel>
-    <AlertAction asChild>
-      <Button onClick={action("action")}>Delete</Button>
-    </AlertAction>
-  </Alert>
-);
+export const Controlled: Story = {
+  parameters: { docs: { disable: true } },
+  args: {
+    ...Default.args,
+    open: true,
+    title: "Are you sure?",
+  },
+};
