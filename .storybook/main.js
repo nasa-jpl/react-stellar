40c38c5f7d3ac4b82810b04ffc06b09600b05b06
AaronPlave <aaronplave@gmail.com>
2023-05-15 11:22:12 -0700

    Formatting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,25 @@
 const path = require("path");
 module.exports = {
   stories: ["../src"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-scss", "storybook-dark-mode"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
+    "storybook-dark-mode",
+  ],
   // Absolute imports
-  webpackFinal: async config => {
-    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, "../src")];
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, "../src"),
+    ];
     return config;
   },
   framework: {
     name: "@storybook/react-webpack5",
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
