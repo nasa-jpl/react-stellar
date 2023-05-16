79545efe7a331972d20880dd02fd13d8a957807c
AaronPlave <aaronplave@gmail.com>
2023-05-16 07:05:46 -0700

    Popover updates

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -1,90 +1,78 @@
 import { action } from "@storybook/addon-actions";
-import { StoryFn, Meta } from "@storybook/react";
-
+import { Meta, StoryObj } from "@storybook/react";
 import { Popover, PopoverClose } from "components/Popover";
 import { Button, Checkbox } from "index";
 
-export default {
-  component: Popover,
-  parameters: {
-    docs: {
-      description: {
-        component: "Overlay content, triggered by a button.",
-      },
-    },
-  },
-} as Meta<typeof Popover>;
+export default { component: Popover } as Meta<typeof Popover>;
+type Story = StoryObj<typeof Popover>;
 
-const Template: StoryFn<typeof Popover> = (args) => (
-  <div style={{ height: "500px" }}>
-    <Popover {...args} />
-  </div>
-);
-
-const popoverChildren = (
-  <>
-    <div>
-      <div style={{ marginBottom: "8px" }} className="st-typography-small-caps">
-        Option Group 1
-      </div>
-      <Checkbox label="Group1 1" />
-      <Checkbox label="Group1 2" />
-      <Checkbox label="Group1 3" />
-      <Checkbox label="Group1 4" />
-    </div>
-    <br />
-    <div>
-      <div style={{ marginBottom: "8px" }} className="st-typography-small-caps">
-        Option Group 2
-      </div>
-      <Checkbox label="Group2 1" />
-      <Checkbox label="Group2 2" />
-      <Checkbox label="Group2 3" />
-      <Checkbox label="Group2 4" />
-    </div>
-    <br />
-    <PopoverClose asChild>
-      <Button variant="secondary" style={{ width: "100%" }}>
-        Close
-      </Button>
-    </PopoverClose>
-  </>
-);
-
-export const Interactive = {
-  render: Template,
-
+export const Default: Story = {
   args: {
-    onOpenChange: action("open changed"),
-    children: popoverChildren,
     trigger: <Button>Open Popover</Button>,
+    children: (
+      <>
+        <div>
+          <div
+            style={{ marginBottom: "8px" }}
+            className="st-typography-small-caps"
+          >
+            Option Group 1
+          </div>
+          <Checkbox label="Group1 1" />
+          <Checkbox label="Group1 2" />
+          <Checkbox label="Group1 3" />
+          <Checkbox label="Group1 4" />
+        </div>
+        <br />
+        <div>
+          <div
+            style={{ marginBottom: "8px" }}
+            className="st-typography-small-caps"
+          >
+            Option Group 2
+          </div>
+          <Checkbox label="Group2 1" />
+          <Checkbox label="Group2 2" />
+          <Checkbox label="Group2 3" />
+          <Checkbox label="Group2 4" />
+        </div>
+        <br />
+        <PopoverClose asChild>
+          <Button variant="secondary" style={{ width: "100%" }}>
+            Close
+          </Button>
+        </PopoverClose>
+      </>
+    ),
   },
+  decorators: [
+    (Story) => (
+      <div style={{ height: "500px" }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
-export const OverridingRadixProps = () => (
-  <Popover
+export const OverridingRadixProps: Story = {
+  args: {
+    ...Default.args,
     // All other props are spread to the Radix Popover Root.
     // See https://www.radix-ui.com/docs/primitives/components/popover#root
     // for a full list of Root props
-    modal // Radix Popover Root prop example
-    onOpenChange={action("open changed")} // Radix Popover Root prop example
+    modal: true, // Radix Popover Root prop example
+    onOpenChange: action("open changed"), // Radix Popover Root prop example
     // Control the majority of the Popover behaviors by setting Radix Popover Content props.
     // See https://www.radix-ui.com/docs/primitives/components/popover#content
     // for a full list of Content props.
     // Common props include "side", "sideOffset", "align", and "avoidCollisions".
-    contentProps={{
+    contentProps: {
       sideOffset: 18,
       alignOffset: 32,
       onInteractOutside: (evt) => evt.preventDefault(),
       onPointerDownOutside: (evt) => evt.preventDefault(),
       onEscapeKeyDown: (evt) => evt.preventDefault(),
-    }}
-    trigger={<Button>Open Popover</Button>}
-  >
-    {popoverChildren}
-  </Popover>
-);
-
-export const ReactJSXExample = () => (
-  <Popover trigger={<Button>Open Popover</Button>}>{popoverChildren}</Popover>
-);
+    },
+    trigger: <Button>Open Popover</Button>,
+  },
+};
