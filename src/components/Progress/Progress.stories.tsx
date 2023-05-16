f81c4bccf78881d936090fe130130cb1b872a0c5
AaronPlave <aaronplave@gmail.com>
2023-05-16 07:08:38 -0700

    Progress component updates

--- src/components/Progress/Progress.stories.tsx
+++ src/components/Progress/Progress.stories.tsx
@@ -1,11 +1,9 @@
-import { StoryFn, Meta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { Progress } from "components/Progress";
 
-export default {
-  component: Progress,
-} as Meta<typeof Progress>;
+export default { component: Progress } as Meta<typeof Progress>;
 
-export const Default = {
+export const Default: StoryObj = {
   args: {
     progress: 50,
   },
